63de2adc7a5aa26fcfc649e21a7cf3c27ee8c9f3
SnehaReddy17 <119933131+SnehaReddy17@users.noreply.github.com>
2023-06-13 12:13:53 +0530

    Add files via upload

---
+++ literals.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+class Literals
+{
+	public static void main(String args[])
+	{
+		int a=10;
+		int b=0457;
+		int c=0xface;
+		float d=123.456f;
+		double e =123.456;
+		float f=1.2e3f;
+		char g='a';
+		char h=97;
+		char i=0xface;
+		char j=0777;
+		char k=65535;
+		char l='\u0061';
+		char m='\'';
+		int n=0b1011;
+		double o=1_23_456.789;
+		float p=123.45f;
+		String s="Hello";
+		System.out.println("a"+a);
+		System.out.println("b"+b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println("h"+h);
+		System.out.println("i"+i);
+		System.out.println("j"+j);
+		System.out.println("k"+k);
+		System.out.println("l"+l);
+		System.out.println("m"+m);
+		System.out.println("n"+n);
+		System.out.println("o"+o);
+		System.out.println("p"+p);
+		System.out.println("s"+s);
+
+	}
+}
